29de2f28a03b05c7e4c76ba8444366429684ccd8
sivasurya2347 <pnds2003@gmail.com>
2024-06-13 22:17:30 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,48 @@
-import './App.css'
-import React from 'react'
-import Head from "./components/Head.js"
-import Watch from './components/Watch.js'
-import Body from './components/Body.js'
-import { Provider } from 'react-redux'
-import Store from './redux/Store.js'
-import { createBrowserRouter,RouterProvider } from 'react-router-dom';
-import Error from './components/Error.js'
-import MainContainer from './components/MainContainer.js'
+import './App.css';
+import React from 'react';
+import Head from "./components/Head.js";
+import Watch from './components/Watch.js';
+import Body from './components/Body.js';
+import { Provider } from 'react-redux';
+import Store from './redux/Store.js';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Error from './components/Error.js';
+import MainContainer from './components/MainContainer.js';
+import SearchVideo from './components/SearchVideo.js';
 
 const router = createBrowserRouter([
    {
-      path : "/",
-      element : <Body />,
-      children : [
+      path: "/",
+      element: (
+          <>
+              <Head />
+              <Body />
+          </>
+      ),
+      children: [
          {
-            path : "/",
-            element : <MainContainer/>
+            path: "/",
+            element: <MainContainer />
          },
          {
-            path : "/watch",
-            element : <Watch/>
+            path: "/watch",
+            element: <Watch />
+         },
+         {
+            path: "/results",
+            element: <SearchVideo />
          }
-   ],
-   errorElement : <Error/>
+      ],
+      errorElement: <Error />
+   }
+]);
 
-}])
-const App = ()=>{
-    return(
-      <Provider store={Store}>
-       <div>
-          <Head />
-          <RouterProvider router={router} />
-       </div>
-       </Provider>
-    )
+const App = () => {
+    return (
+        <Provider store={Store}>
+            <RouterProvider router={router} />
+        </Provider>
+    );
 }
 
 export default App;
