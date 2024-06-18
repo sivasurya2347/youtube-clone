f55339f2ce2041b4088a968f61b3c7e4d8cf4fc7
sivasurya2347 <pnds2003@gmail.com>
2024-06-18 22:32:47 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,50 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
+import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Store from './redux/Store.js';
 import Head from "./components/Head.js";
 import Watch from './components/Watch.js';
 import Body from './components/Body.js';
-import { Provider } from 'react-redux';
-import Store from './redux/Store.js';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Error from './components/Error.js';
 import MainContainer from './components/MainContainer.js';
-import SearchVideo from './components/SearchVideo.js';
-
-const router = createBrowserRouter([
-   {
-      path: "/",
-      element: (
-          <>
-              <Head />
-              <Body />
-          </>
-      ),
-      children: [
-         {
-            path: "/",
-            element: <MainContainer />
-         },
-         {
-            path: "/watch",
-            element: <Watch />
-         },
-         {
-            path: "/results",
-            element: <SearchVideo />
-         }
-      ],
-      errorElement: <Error />
-   }
-]);
+import SearchResult from './components/SearchResult.js';
 
 const App = () => {
+    const [videosearch, setvideosearch] = useState([]);
+
+    const handleSearch = (data) => {
+        setvideosearch(data);
+        console.log(data)
+    }
+
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: (
+                <>
+                    <Head onSearch={handleSearch}/>
+                    <Body />
+                </>
+            ),
+            children: [
+                {
+                    path: "/",
+                    element: <MainContainer />
+                },
+                {
+                    path: "/watch",
+                    element: <Watch />
+                },
+                {
+                    path: "/results",
+                    element: <SearchResult videosearch={videosearch} />
+                }
+            ],
+            errorElement: <Error />
+        }
+    ]);
+
     return (
         <Provider store={Store}>
             <RouterProvider router={router} />
